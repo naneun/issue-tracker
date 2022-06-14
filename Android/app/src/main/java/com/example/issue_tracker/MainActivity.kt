a7671af958afb20620f979e5cade8ffed76f3600
ramkid91 <ramkid91@gmail.com>
2022-06-14 15:54:12 +0900

    feat:  화면 이동 구조 설계, 바텀 내비게이션 연결
    - 이슈, 라벨, 마일스톤, 내계정은 바깥쪽 fragmentcontainer
    - 이슈 내부에 fragmentContainer가 하나 더 생성해, 디테일, 작성, 편집 화면등 구성예정
    - jetpack naviagtion과 bottom navigation 연결

--- Android/app/src/main/java/com/example/issue_tracker/MainActivity.kt
+++ Android/app/src/main/java/com/example/issue_tracker/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.issue_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.issue_tracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner= this
+        setContentView(binding.root)
+        setupNav()
+    }
+
+    private fun setupNav() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.fragment_container)?.findNavController()
+
+        navController?.let {
+            binding.bottomNavigationView.setupWithNavController(it)
+        }
     }
 }

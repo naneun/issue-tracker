84b7d429216ebe9d53fe72be0f2b9cf1f3435785
ramkid91 <ramkid91@gmail.com>
2022-06-16 16:30:49 +0900

    feat:  팝업 윈도위 위치조정,  복사, 자르기, 붙이기 액션 등록
    - editText 중앙 상단에 팝업 윈도우가 뜨도록 조정
    - 붙이기버튼은 저장되어있는 text를 editText에 append
    - 복사 버튼은 editText에 현재 저장되어있는 text를 copyText에 복사
    - 자르기 버튼은 editTextdㅔ 현재 저장되어 있는 text를 copyText에 복사후, 현재 editText를 비움

--- Android/app/src/main/java/com/example/issue_tracker/ui/issue/write/IssueWriteFragment.kt
+++ Android/app/src/main/java/com/example/issue_tracker/ui/issue/write/IssueWriteFragment.kt
@@ -1,11 +1,12 @@
 package com.example.issue_tracker.ui.issue.write
 
-import android.app.ActionBar
 import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.PopupWindow
 import androidx.databinding.DataBindingUtil
@@ -18,7 +19,13 @@ import io.noties.markwon.image.ImagesPlugin
 
 class IssueWriteFragment : Fragment() {
     private var markDownFlag = false
+    private lateinit var popupWindow: PopupWindow
     private var beforeChangedText = ""
+    private var copyText=""
+    private lateinit var cutButton:Button
+    private lateinit var copyButton:Button
+    private lateinit var insertPhotoButton:Button
+    private lateinit var pasteButton:Button
     private lateinit var binding: FragmentIssueWriteBinding
     private lateinit var markOne: Markwon
     override fun onCreateView(
@@ -33,24 +40,61 @@ class IssueWriteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         markOne = Markwon.builder(requireContext()).usePlugin(ImagesPlugin.create()).build()
         displayMarkDownPreview()
-        binding.etIssueWriteContent.setOnClickListener {
+        setLongClickPopupWindow(view)
 
+    }
+
+    private fun setLongClickPopupWindow(view: View) {
+        binding.etIssueWriteContent.setOnLongClickListener {
             displayPopup(view)
+            true
+        }
+    }
+
+    private fun addCutAction(){
+        cutButton.setOnClickListener {
+            copyText= binding.etIssueWriteContent.text.toString()
+            binding.etIssueWriteContent.setText("")
         }
 
     }
 
+    private fun addCopyAction(){
+        copyButton.setOnClickListener {
+            copyText= binding.etIssueWriteContent.text.toString()
+        }
+    }
 
-    private fun displayPopup(view: View){
-        val popupWindow= PopupWindow(view, LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT)
-        println("call Here")
-        val inflater= requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        val popupView= inflater.inflate(R.layout.issue_popup_window, null)
-        popupWindow.contentView= popupView
-        popupWindow.isTouchable= true
-        popupWindow.isFocusable=true
-        popupWindow.isOutsideTouchable=true
-        popupWindow.showAsDropDown(view)
+    private fun addPasteAction(){
+        pasteButton.setOnClickListener {
+            binding.etIssueWriteContent.append(copyText)
+        }
+    }
+
+    private fun displayPopup(view: View) {
+        popupWindow = PopupWindow(view, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT)
+        val inflater = requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val popupView = inflater.inflate(R.layout.issue_popup_window, null)
+        findMenuButton(popupView)
+        addMenuEventListener()
+        popupWindow.contentView = popupView
+        popupWindow.isTouchable = true
+        popupWindow.isFocusable = true
+        popupWindow.isOutsideTouchable = true
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, -200);
+    }
+
+    private fun findMenuButton(popupView:View){
+        copyButton= popupView.findViewById(R.id.btn_copy)
+        cutButton= popupView.findViewById(R.id.btn_cut)
+        insertPhotoButton= popupView.findViewById(R.id.btn_insert_photo)
+        pasteButton = popupView.findViewById(R.id.btn_paste)
+    }
+
+    private fun addMenuEventListener(){
+        addCopyAction()
+        addCutAction()
+        addPasteAction()
     }
 
     private fun displayMarkDownPreview() {

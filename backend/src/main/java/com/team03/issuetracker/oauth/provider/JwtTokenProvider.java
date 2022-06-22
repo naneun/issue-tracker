6c2ada03be5718e77d7f14688c359b74ad6f9641
honeySleepr <gruzzimo@naver.com>
2022-06-22 13:55:24 +0900

    ♻️ JWT key 생성방식 변경, 환경 변수 수정

--- backend/src/main/java/com/team03/issuetracker/oauth/provider/JwtTokenProvider.java
+++ backend/src/main/java/com/team03/issuetracker/oauth/provider/JwtTokenProvider.java
@@ -4,54 +4,51 @@ import com.team03.issuetracker.common.domain.Member;
 import com.team03.issuetracker.oauth.properties.JwtProperties;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-
 import org.springframework.stereotype.Component;
 
 @Component
 public class JwtTokenProvider {
 
-    private final String issuer;
+	private final String issuer;
 
-    private final String secretKey;
+	private final String secretKey;
 
-    public JwtTokenProvider(JwtProperties properties) {
-        this.issuer = properties.getIssuer();
-        this.secretKey = properties.getSecretKey();
-    }
+	public JwtTokenProvider(JwtProperties properties) {
+		this.issuer = properties.getIssuer();
+		this.secretKey = properties.getSecretKey();
+	}
 
-    public String makeJwtAccessToken(Member member) {
+	public String makeJwtAccessToken(Member member) {
+		return Jwts.builder()
+			.setAudience(member.getId().toString())
+			.setIssuer(issuer)
+			.setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))
+			.setExpiration(Timestamp.valueOf(LocalDateTime.now().plusHours(1L)))
+			.signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)))
+			.compact();
 
-        return Jwts.builder()
-                .setAudience(member.getId().toString())
-                .setIssuer(issuer)
-                .setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))
-                .setExpiration(Timestamp.valueOf(LocalDateTime.now().plusHours(1L)))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
+	}
 
-    }
+	public String makeJwtRefreshToken(Member member) {
+		return Jwts.builder()
+			.setAudience(member.getId().toString())
+			.setIssuer(issuer)
+			.setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))
+			.setExpiration(Timestamp.valueOf(LocalDateTime.now().plusWeeks(2L)))
+			.signWith(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)))
+			.compact();
 
-    public String makeJwtRefreshToken(Member member) {
+	}
 
-        return Jwts.builder()
-                .setAudience(member.getId().toString())
-                .setIssuer(issuer)
-                .setIssuedAt(Timestamp.valueOf(LocalDateTime.now()))
-                .setExpiration(Timestamp.valueOf(LocalDateTime.now().plusWeeks(2L)))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
-                .compact();
-
-    }
-
-    public Claims verifyToken(String jwtToken) {
-
-        return Jwts.parser()
-                .setSigningKey(secretKey)
-                .parseClaimsJws(jwtToken)
-                .getBody();
-    }
+	public Claims verifyToken(String jwtToken) {
+		return Jwts.parserBuilder()
+			.setSigningKey(Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey)))
+			.build()
+			.parseClaimsJws(jwtToken)
+			.getBody();
+	}
 }

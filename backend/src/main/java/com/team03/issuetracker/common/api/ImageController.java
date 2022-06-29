d82991022b68a36c78e90a3b9c0ec1fa236f301a
naneun <rmtlrksdp@naver.com>
2022-06-29 17:29:24 +0900

    ✨ 이슈 등록 시 assignee 로 등록 가능한 사용자 목록  출력 API 추가

--- backend/src/main/java/com/team03/issuetracker/common/api/ImageController.java
+++ backend/src/main/java/com/team03/issuetracker/common/api/ImageController.java
@@ -7,12 +7,14 @@ import org.apache.tomcat.util.http.fileupload.FileUploadException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/images")
 public class ImageController {
 
 	private final ImageUploader s3Uploader;
@@ -23,7 +25,7 @@ public class ImageController {
 		produces = "application/json",
 		response = String.class
 	)
-	@PostMapping("/images")
+	@PostMapping
 	public ResponseEntity<String> upload(@RequestPart("image") MultipartFile multipartFile)
 		throws FileUploadException {
 
@@ -35,7 +37,7 @@ public class ImageController {
 		notes = "AWS S3 버킷에 업로드한 이미지를 삭제한다.",
 		produces = "application/json"
 	)
-	@DeleteMapping("/images")
+	@DeleteMapping
 	public ResponseEntity<Void> delete(String imageKey) {
 		s3Uploader.deleteImage(imageKey);
 		return ResponseEntity.ok().build();

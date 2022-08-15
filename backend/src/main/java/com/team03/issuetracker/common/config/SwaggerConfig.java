acb0f22ddee939830fa77847640786f55e2e4714
honeySleepr <gruzzimo@naver.com>
2022-08-15 16:38:49 +0900

    💡 Spring Boot Starter Actuator 의존성 및 설정 코드 추가

--- backend/src/main/java/com/team03/issuetracker/common/config/SwaggerConfig.java
+++ backend/src/main/java/com/team03/issuetracker/common/config/SwaggerConfig.java
@@ -3,15 +3,23 @@ package com.team03.issuetracker.common.config;
 import com.fasterxml.classmate.TypeResolver;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.CorsEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.endpoint.web.WebEndpointProperties;
+import org.springframework.boot.actuate.autoconfigure.web.server.ManagementPortType;
+import org.springframework.boot.actuate.endpoint.ExposableEndpoint;
+import org.springframework.boot.actuate.endpoint.web.*;
+import org.springframework.boot.actuate.endpoint.web.annotation.ControllerEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.annotation.ServletEndpointsSupplier;
+import org.springframework.boot.actuate.endpoint.web.servlet.WebMvcEndpointHandlerMapping;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.domain.Pageable;
+import org.springframework.util.StringUtils;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.AlternateTypeRules;
@@ -29,6 +37,34 @@ public class SwaggerConfig {
 		return new ApiInfoBuilder().title("Issue-Tracker API")
 			.description("team-03 API Docs").build();
 	}
+
+	/* Actuator 의존성 추가 시 Failed to start bean 'documentationPluginsBootstrapper' 에러 발생하는 문제 해결을 위해 추가 */
+	@Bean
+	public WebMvcEndpointHandlerMapping webEndpointServletHandlerMapping(WebEndpointsSupplier webEndpointsSupplier,
+		ServletEndpointsSupplier servletEndpointsSupplier, ControllerEndpointsSupplier controllerEndpointsSupplier,
+		EndpointMediaTypes endpointMediaTypes, CorsEndpointProperties corsProperties,
+		WebEndpointProperties webEndpointProperties, Environment environment) {
+		List<ExposableEndpoint<?>> allEndpoints = new ArrayList();
+		Collection<ExposableWebEndpoint> webEndpoints = webEndpointsSupplier.getEndpoints();
+		allEndpoints.addAll(webEndpoints);
+		allEndpoints.addAll(servletEndpointsSupplier.getEndpoints());
+		allEndpoints.addAll(controllerEndpointsSupplier.getEndpoints());
+		String basePath = webEndpointProperties.getBasePath();
+		EndpointMapping endpointMapping = new EndpointMapping(basePath);
+		boolean shouldRegisterLinksMapping = this.shouldRegisterLinksMapping(webEndpointProperties, environment,
+			basePath);
+		return new WebMvcEndpointHandlerMapping(endpointMapping, webEndpoints, endpointMediaTypes,
+			corsProperties.toCorsConfiguration(), new EndpointLinksResolver(allEndpoints, basePath),
+			shouldRegisterLinksMapping, null);
+	}
+
+	private boolean shouldRegisterLinksMapping(WebEndpointProperties webEndpointProperties, Environment environment,
+		String basePath) {
+		return webEndpointProperties.getDiscovery().isEnabled() && (StringUtils.hasText(basePath) ||
+																	ManagementPortType.get(environment)
+																		.equals(ManagementPortType.DIFFERENT));
+	}
+	/*-------------------------------------------------------------------------------------------------*/
 
 	@Bean
 	public Docket swaggerApi() {
